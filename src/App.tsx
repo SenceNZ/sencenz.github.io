ff788d3a3d0613aa328cddf915ffe5642262fc0b
sam <sam@chatrlyst.com>
2023-04-25 21:49:51 +1200

    flex the images and do some mobile friendly things, and a google sheet link

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ArrowRightCircleIcon } from "@heroicons/react/20/solid";
+
 function App() {
   return (
     <div className="bg-gradient-to-b from-cyan-50 via-cyan-100 to-cyan-200 text-slate-700">
       <div className="container mx-auto min-h-screen px-8 py-12">
         <img className="w-80" src="/logo_w_text.png" />
-        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-y-4 lg:gap-y-0">
-          <div className="p-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 lg:gap-y-0">
+          <div className="p-6 lg:mt-4 lg:mr-4">
             <div className="text-5xl font-semibold mb-8">
               <p>
                 Meet <span className="text-cyan-500">Chatrlyst</span>.
@@ -12,30 +14,29 @@ function App() {
               <span className="text-cyan-500">AI-Powered</span> Insights for
               Content Creators.
             </div>
-            <div className="text-xl font-semibold mb-8">
+            <div className="text-xl font-semibold mb-8 lg:mb-16">
               Engage with your audience like never before. Revolutionise your
               content creation with our AI-powered analytics tool. Coming soon.
             </div>
             <a
-              className="text-xl font-semibold"
-              href="mailto:hello@chatrlyst.com"
+              className="text-xl text-white bg-cyan-500 rounded-full pl-4 pr-3 py-2.5"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSehYzi8HmT2OrheJ4aoD3dg8ZFawyuLR9uhYVs-yO3dd6QAjA/viewform"
+              target="_blank"
             >
-              <span className="text-4xl">📧</span> hello@chatrlyst.com
+              <span className="align-bottom mr-1">Join the waitlist </span>
+              <ArrowRightCircleIcon className="w-7 inline-block align-bottom" />
             </a>
           </div>
-          <div className="flex flex-wrap justify-center gap-8">
-            <img
-              className="w-72 rounded-3xl drop-shadow-xl bg-white"
-              src="/chart_1.png"
-            />
-            <img
-              className="w-72 rounded-3xl drop-shadow-xl bg-white"
-              src="/chart_2.png"
-            />
-            <img
-              className="w-72 rounded-3xl drop-shadow-xl bg-white col-span-2"
-              src="/chart_3.png"
-            />
+          <div className="flex flex-wrap justify-center gap-12">
+            <div className="md:basis-[calc(50%-1.5rem)] rounded-3xl drop-shadow-xl bg-white">
+              <img src="/chart_1.png" />
+            </div>
+            <div className="md:basis-[calc(50%-1.5rem)] rounded-3xl drop-shadow-xl bg-white">
+              <img src="/chart_2.png" />
+            </div>
+            <div className="md:basis-[calc(50%-1.5rem)] rounded-3xl drop-shadow-xl bg-white">
+              <img src="/chart_3.png" />
+            </div>
           </div>
         </div>
         <div className="sticky mt-12 top-[100vh] flex justify-center">
